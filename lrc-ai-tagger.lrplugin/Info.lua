cd6f1b640984693b126dee0ed48bbeac36cbcdb6
Bastian Machek <16717398+bmachek@users.noreply.github.com>
2024-10-04 07:34:07 +0200

    Added missing strings in LOC.

--- lrc-ai-tagger.lrplugin/Info.lua
+++ lrc-ai-tagger.lrplugin/Info.lua
@@ -3,7 +3,7 @@ return {
 	LrSdkVersion = 3.0,
 	LrSdkMinimumVersion = 3.0,
 	LrToolkitIdentifier = 'lrc-ai-tagger',
-	LrPluginName = "LRC AI Tagger",
+	LrPluginName = LOC "$$$/lrc-ai-tagger/Info/PluginName=Lightroom AI assistant",
 	LrInitPlugin = "Init.lua",
 	LrPluginInfoProvider = 'PluginInfo.lua',
 	LrPluginInfoURL = 'https://github.com/bmachek/lrc-ai-tagger',
