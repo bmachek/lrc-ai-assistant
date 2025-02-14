56c436401029ab53ee9d8b412b7cdddbe26ad14e
Bastian Machek <16717398+bmachek@users.noreply.github.com>
2025-02-14 08:02:12 +0100

    Added gemini-2.0-pro-exp for testing

--- lrc-ai-assistant.lrplugin/Defaults.lua
+++ lrc-ai-assistant.lrplugin/Defaults.lua
@@ -90,6 +90,7 @@ Defaults.aiModels = {
     { title = "Google Gemini Flash 1.5", value = "gemini-1.5-flash" },
     { title = "Google Gemini Pro 1.5", value = "gemini-1.5-pro" },
     { title = "Google Gemini Flash 2.0", value = "gemini-2.0-flash" },
+    { title = "Google Gemini Pro 2.0 (experimental)", value = "gemini-2.0-pro-exp" },
     { title = "ChatGPT-4", value = "gpt-4o" },
     { title = "ChatGPT-4 Mini", value = "gpt-4o-mini" },
     { title = 'Ollama deepseek-r1', value = 'ollama-deepseek-r1' },
@@ -105,6 +106,7 @@ Defaults.baseUrls = {}
 Defaults.baseUrls['gemini-1.5-flash'] = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key='
 Defaults.baseUrls['gemini-1.5-pro'] = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro:generateContent?key='
 Defaults.baseUrls['gemini-2.0-flash'] = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-exp:generateContent?key='
+Defaults.baseUrls['gemini-2.0-pro-exp'] = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-pro-exp-02-05:generateContent?key='
 Defaults.baseUrls['gpt-4o'] = 'https://api.openai.com/v1/chat/completions'
 Defaults.baseUrls['gpt-4o-mini'] = 'https://api.openai.com/v1/chat/completions'
 Defaults.baseUrls['ollama'] = 'http://localhost:11434/v1/chat/completions'
@@ -113,6 +115,9 @@ Defaults.pricing = {}
 Defaults.pricing["gemini-1.5-pro"] = {}
 Defaults.pricing["gemini-1.5-pro"].input = 3.5 / 1000000
 Defaults.pricing["gemini-1.5-pro"].output= 10.5 / 1000000
+Defaults.pricing["gemini-2.0-pro-exp"] = {}
+Defaults.pricing["gemini-2.0-pro-exp"].input = 3.5 / 1000000
+Defaults.pricing["gemini-2.0-pro-exp"].output= 10.5 / 1000000
 Defaults.pricing["gemini-1.5-flash"] = {}
 Defaults.pricing["gemini-1.5-flash"].input = 0.075 / 1000000
 Defaults.pricing["gemini-1.5-flash"].output= 0.3 / 1000000
