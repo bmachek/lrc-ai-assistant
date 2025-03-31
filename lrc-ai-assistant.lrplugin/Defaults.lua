73bb0332bbf203c2eca272c3c75989ea4b5bec7a
Bastian Machek <16717398+bmachek@users.noreply.github.com>
2025-03-31 14:52:11 +0200

    Updated Gemini models in config.

--- lrc-ai-assistant.lrplugin/Defaults.lua
+++ lrc-ai-assistant.lrplugin/Defaults.lua
@@ -69,10 +69,10 @@ Defaults.targetDataFields = {
 }
 
 Defaults.aiModels = {
-    { title = "Google Gemini Flash 1.5", value = "gemini-1.5-flash" },
     { title = "Google Gemini Pro 1.5", value = "gemini-1.5-pro" },
     { title = "Google Gemini Flash 2.0", value = "gemini-2.0-flash" },
-    { title = "Google Gemini Pro 2.0 (experimental)", value = "gemini-2.0-pro-exp" },
+    { title = "Google Gemini Flash 2.0 Lite", value = "gemini-2.0-flash-lite" },
+    { title = "Google Gemini Pro 2.5 (experimental)", value = "gemini-2.5-pro-exp-03-25" },
     { title = "ChatGPT-4", value = "gpt-4o" },
     { title = "ChatGPT-4 Mini", value = "gpt-4o-mini" },
     { title = 'Ollama deepseek-r1', value = 'ollama-deepseek-r1' },
@@ -86,27 +86,27 @@ Defaults.exportSizes = {
 }
 
 Defaults.baseUrls = {}
-Defaults.baseUrls['gemini-1.5-flash'] = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key='
 Defaults.baseUrls['gemini-1.5-pro'] = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro:generateContent?key='
-Defaults.baseUrls['gemini-2.0-flash'] = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-exp:generateContent?key='
-Defaults.baseUrls['gemini-2.0-pro-exp'] = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-pro-exp-02-05:generateContent?key='
+Defaults.baseUrls['gemini-2.0-flash'] = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key='
+Defaults.baseUrls['gemini-2.0-flash-lite'] = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-lite:generateContent?key='
+Defaults.baseUrls['gemini-2.5-pro-exp-03-25'] = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-pro-exp-03-25:generateContent?key='
 Defaults.baseUrls['gpt-4o'] = 'https://api.openai.com/v1/chat/completions'
 Defaults.baseUrls['gpt-4o-mini'] = 'https://api.openai.com/v1/chat/completions'
 Defaults.baseUrls['ollama'] = 'http://localhost:11434/v1/chat/completions'
 
 Defaults.pricing = {}
 Defaults.pricing["gemini-1.5-pro"] = {}
-Defaults.pricing["gemini-1.5-pro"].input = 3.5 / 1000000
-Defaults.pricing["gemini-1.5-pro"].output= 10.5 / 1000000
-Defaults.pricing["gemini-2.0-pro-exp"] = {}
-Defaults.pricing["gemini-2.0-pro-exp"].input = 3.5 / 1000000
-Defaults.pricing["gemini-2.0-pro-exp"].output= 10.5 / 1000000
-Defaults.pricing["gemini-1.5-flash"] = {}
-Defaults.pricing["gemini-1.5-flash"].input = 0.075 / 1000000
-Defaults.pricing["gemini-1.5-flash"].output= 0.3 / 1000000
+Defaults.pricing["gemini-1.5-pro"].input = 1.25 / 1000000
+Defaults.pricing["gemini-1.5-pro"].output= 5 / 1000000
+Defaults.pricing["gemini-2.5-pro-exp-03-25"] = {}
+Defaults.pricing["gemini-2.5-pro-exp-03-25"].input = 3.5 / 1000000
+Defaults.pricing["gemini-2.5-pro-exp-03-25"].output= 10.5 / 1000000
 Defaults.pricing["gemini-2.0-flash"] = {}
-Defaults.pricing["gemini-2.0-flash"].input = 0.075 / 1000000
-Defaults.pricing["gemini-2.0-flash"].output= 0.3 / 1000000
+Defaults.pricing["gemini-2.0-flash"].input = 0.1 / 1000000
+Defaults.pricing["gemini-2.0-flash"].output= 0.4 / 1000000
+Defaults.pricing["gemini-2.0-flash-lite"] = {}
+Defaults.pricing["gemini-2.0-flash-lite"].input = 0.075 / 1000000
+Defaults.pricing["gemini-2.0-flash-lite"].output= 0.3 / 1000000
 Defaults.pricing["gpt-4o"] = {}
 Defaults.pricing["gpt-4o"].input = 2.5 / 1000000
 Defaults.pricing["gpt-4o"].output= 10 / 1000000
